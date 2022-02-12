3f5e4cec2be1134f3558bbbbd3f2869d86fabcfa
Callum Lees <hi@c-l.dev>
2022-02-12 18:07:06 +0000

    Base React and TailwindCSS setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex h-screen mx-auto justify-center items-center'>
+      <h1 className='text-2xl'>Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
