aa760d018fb94e74359681deb78d7051115c315d
Callum Lees <hi@c-l.dev>
2022-07-02 12:44:24 +0100

    Updated Hero.js text

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,22 +1,42 @@
 import Typewriter from 'typewriter-effect';
 
 export const Hero = () => {
-    return (
-        <>
-            {/* <Navbar /> */}
-                <div className='flex h-screen mx-auto justify-center items-center max-w-2xl'>
-                    <h1 className='text-6xl md:text-8xl font-chasm'>
-                        <Typewriter
-                            options={{
-                            strings: ['we are', 'mnml'],
-                            autoStart: true,
-                            loop: true,
-                            }}
-                        />
-                    </h1>
-                </div>
-        </>
-    );
+
+  return (
+
+    <>
+
+      <div className='flex h-screen mx-auto justify-center items-center max-w-8xl'>
+
+        <div className="flex-col">
+
+          <h1 className='text-6xl md:text-9xl text-gray-900'>
+
+            <Typewriter
+              options={{
+                strings: ['Minimal.', 'Simple.', 'Creative.'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+
+          </h1>
+
+          <div className="mt-20 flex-col mx-auto uppercase text-center text-gray-900">
+
+            <p className="animate-bounce">&darr;</p>
+            <p className="text-xs">Scroll down</p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </>
+
+  );
+
 }
 
 export default Hero;
